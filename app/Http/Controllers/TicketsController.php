d102a8f133d31c8eeab2664a4ce67dbc339d370a
= <=>
2016-01-03 15:46:27 +0800

    Removing unused declarations and adding construct method

--- app/Http/Controllers/TicketsController.php
+++ app/Http/Controllers/TicketsController.php
@@ -6,25 +6,31 @@ use App\Backlog;
 use App\Http\Requests\TicketRequest;
 use App\Ticket;
 use App\User;
-use App\Comment;
-use Carbon\Carbon;
 
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Facades\Session;
 
 class TicketsController extends Controller
 {
+
+    /**
+     * @var Ticket
+     */
+    private $ticket;
+
+    public  function __construct(Ticket $ticket)
+    {
+        $this->ticket = $ticket;
+    }
+
     /**
      * Displays all Tickets
      *
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return View
      */
     public function all()
     {
-        $tickets = Ticket::latest()->paginate();
+        $tickets = $this->ticket->orderBy('id', 'asc')->paginate();
         $header = 'All Tickets';
         return view('tickets.index', compact('tickets', 'header'));
     }
@@ -33,63 +39,68 @@ class TicketsController extends Controller
      * Displays the Tickets by the given User id
      *
      * @param $id
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return View
      */
     public function tickets_by_user($id)
     {
-        $tickets = Ticket::where('user_id', $id)->latest()->paginate(10);
+        $tickets = $this->ticket->whereUserId($id)
+            ->orderBy('id', 'asc')
+            ->paginate(10);
         $header = 'Tickets by ' . User::findOrFail($id)->name;
         return view('tickets.index', compact('tickets', 'header'));
     }
 
     /**
-     * Show a Ticket by Its ID
+     * Show a Ticket by its id
      *
      * @param $id
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return View
      */
     public function show($id)
     {
-        $ticket = Ticket::findOrFail($id);
+        $ticket = $this->ticket->findOrFail($id);
         return view('tickets.show', compact('ticket', 'comments_for_ticket'));
     }
 
     /**
-     * Load a form to edit a given Ticket
+     * Load a form to edit a given Ticket id
      *
      * @param $id
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return View
      */
     public function edit($id)
     {
-        $ticket = Ticket::findOrFail($id);
+        $ticket = $this->ticket->findOrFail($id);
         return view('tickets.edit', compact('ticket'));
     }
+
 
     /**
      * Actual process of updating Ticket
      *
      * @param $id
-     * @param TicketRequest $request
-     * @return \Illuminate\Http\RedirectResponse
+     * @param Request $request
+     * @return RedirectResponse
      */
     public function update($id, Request $request)
     {
-        $ticket = Ticket::findOrFail($id);
+        $ticket = $this->ticket->findOrFail($id);
         $ticket->update($request->all());
         session()->flash('flash_message', 'You have successfully updated the ticket');
-        return redirect()->route('ticket_show', [$ticket]);
+        return redirect()->route('show_ticket', [$ticket]);
     }
 
     /**
-     * Returns the Tickets for a given Backlog ID
+     * Returns the Tickets for a given Backlog id
      *
      * @param $id
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return View
      */
     public function tickets_by_backlog($id)
     {
-        $tickets = Ticket::where('backlog_id', $id)->latest()->paginate(10);
+        $tickets = $this->ticket->whereBacklogId($id)
+            ->orderBy('id', 'asc')
+            ->paginate(10);
         $header = 'Tickets for Backlog ' . Backlog::findOrFail($id)->name;
         return view('tickets.index', compact('tickets', 'header'));
     }
@@ -98,11 +109,13 @@ class TicketsController extends Controller
      * Returns all the Tickets whether they are open/close
      *
      * @param $status
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return View
      */
     public function tickets_by_status($status)
     {
-        $tickets = Ticket::where('status', $status)->latest()->paginate(10);
+        $tickets = $this->ticket->whereStatus($status)
+            ->orderBy('id', 'asc')
+            ->paginate(10);
         $header = 'Tickets that are ' . $status;
         return view('tickets.index', compact('tickets', 'header'));
     }
@@ -111,11 +124,13 @@ class TicketsController extends Controller
      * Tickets for a given type
      *
      * @param $type
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return View
      */
     public function tickets_by_type($type)
     {
-        $tickets = Ticket::where('type', $type)->latest()->paginate(10);
+        $tickets = $this->ticket->whereType($type)
+            ->orderBy('id', 'asc')
+            ->paginate(10);
         $header =  $type . ' tickets';
         return view('tickets.index', compact('tickets', 'header'));
     }
@@ -124,11 +139,13 @@ class TicketsController extends Controller
      * Tickets for a given prio
      *
      * @param $priority
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return View
      */
     public function tickets_by_priority($priority)
     {
-        $tickets = Ticket::where('priority', $priority)->latest()->paginate(10);
+        $tickets = $this->ticket->wherePriority($priority)
+            ->orderBy('id', 'asc')
+            ->paginate(10);
         $header =  $priority . ' priority tickets';
         return view('tickets.index', compact('tickets', 'header'));
     }
@@ -136,7 +153,7 @@ class TicketsController extends Controller
     /**
      * Loads a form to create Ticket
      *
-     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     * @return View
      */
     public function create()
     {
@@ -151,12 +168,12 @@ class TicketsController extends Controller
      * Actual process of storing Ticket
      *
      * @param TicketRequest $request
-     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @return Redirector
      */
     public function store(TicketRequest $request)
     {
         $input = $request->all();
-        Ticket::create($input);
+        $this->ticket->create($input);
 
         return redirect('/tickets')->with([
             'flash_message' => 'You have successfully updated the ticket',
@@ -164,11 +181,18 @@ class TicketsController extends Controller
         ]);
     }
 
+    /**
+     * Delete Ticket
+     *
+     * @param $id
+     * @return RedirectResponse
+     */
     public function destroy($id)
     {
-        $ticket = Ticket::findOrFail($id);
+        $ticket = $this->ticket->findOrFail($id);
         $this->authorize('delete-post', $ticket);
         $ticket->delete();
         return redirect('/tickets')->with('flash_message', 'You have successfully deleted the ticket');
     }
+
 }
