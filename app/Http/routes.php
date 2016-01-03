b0d19ac9f65d3c12ff1d7b4ab3dfb5ae9fb20294
= <=>
2016-01-03 15:45:14 +0800

    Formatting routes for clarity of reading

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,41 +1,100 @@
 <?php
 
+// routes that needs authentication
 Route::group(['middleware' => ['auth']], function()
 {
-    Route::get('/user/profile', 'UsersController@profile');
-    Route::get('/user/settings/', 'UsersController@settings');
-    Route::get('/ticket/create', 'TicketsController@create');
-    Route::put('/ticket/{id}', 'TicketsController@update')
+    Route::get('user/profile', [
+        'as' => 'show_profile',
+        'uses' => 'UsersController@show_profile'
+    ]);
+
+    Route::get('/user/settings/', [
+        'as' => 'user_settings',
+        'uses' => 'UsersController@settings'
+    ]);
+
+    Route::get('/ticket/create', [
+        'as' => 'create_ticket',
+        'uses' => 'TicketsController@create'
+    ]);
+
+    Route::patch('/ticket/{id}', 'TicketsController@update')
         ->where('id', '[0-9]+');
+
     Route::post('/ticket/{id}', 'CommentsController@store')
         ->where('id', '[0-9]+');
-    Route::get('/admin', 'UsersController@admin');
-    Route::delete('/ticket/{id}', 'TicketsController@destroy');
+
+    Route::get('/admin', [
+        'as' => 'admin_area',
+        'uses' => 'UsersController@admin'
+    ]);
+
+    Route::delete('/ticket/{id}', 'TicketsController@destroy')
+        ->where('id', '[0-9]+');
+
 });
 
-Route::get('/', 'TicketsController@all');
-Route::get('/tickets', 'TicketsController@all');
+// global routes (no need for authentication)
+// Route::get('/', [
+//     'as' => 'all_tickets',
+//     'uses' => 'TicketsController@all'
+// ]);
+
+Route::get('/', function() {
+    return view('welcome');
+});
+
+Route::get('/tickets', [
+    'as' => 'all_tickets',
+    'uses' => 'TicketsController@all'
+]);
+
 Route::post('/tickets', 'TicketsController@store');
+
 Route::get('/ticket/{id}', [
-    'as' => 'ticket_show',
+    'as' => 'show_ticket',
     'uses' => 'TicketsController@show'
 ])->where('id', '[0-9]+');
-Route::get('/tickets/user/{id}', 'TicketsController@tickets_by_user')->where('id', '[0-9]+');
-Route::get('/tickets/backlog/{id}', 'TicketsController@tickets_by_backlog')
-    ->where('id', '[0-9]+');
-Route::get('/tickets/status/{status}', 'TicketsController@tickets_by_status')
-    ->where('status', '[A-Za-z]+');
-Route::get('/tickets/type/{type}', 'TicketsController@tickets_by_type')
-    ->where('type', '[A-Za-z]+');
-Route::get('/tickets/priority/{priority}', 'TicketsController@tickets_by_priority')
-    ->where('priority', '[A-Za-z]+');
-Route::get('user/{id}', 'UsersController@show_profile');
 
-// Authentication routes...
-Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::get('/tickets/user/{id}', [
+    'as' => 'tickets_by_user',
+    'uses' => 'TicketsController@tickets_by_user'
+])->where('id', '[0-9]+');
+
+Route::get('/tickets/backlog/{id}', [
+    'as' => 'tickets_by_backlog',
+    'uses' => 'TicketsController@tickets_by_backlog'
+])->where('id', '[0-9]+');
+
+Route::get('/tickets/status/{status}', [
+    'as' => 'tickets_by_status',
+    'uses' => 'TicketsController@tickets_by_status'
+])->where('status', '[A-Za-z]+');
+
+Route::get('/tickets/type/{type}', [
+    'as' => 'tickets_by_type',
+    'uses' => 'TicketsController@tickets_by_type'
+])->where('type', '[A-Za-z]+');
+
+Route::get('/tickets/priority/{priority}', [
+    'as' => 'tickets_by_priority',
+    'uses' => 'TicketsController@tickets_by_priority'
+])->where('priority', '[A-Za-z]+');
+
+// Authentication routes
+Route::get('auth/login', [
+    'as' => 'login_path',
+    'uses' => 'Auth\AuthController@getLogin'
+]);
 Route::post('auth/login', 'Auth\AuthController@postLogin');
-Route::get('auth/logout', 'Auth\AuthController@getLogout');
+Route::get('auth/logout', [
+    'as' => 'logout_path',
+    'uses' => 'Auth\AuthController@getLogout'
+]);
 
-// Registration routes...
-Route::get('auth/register', 'Auth\AuthController@getRegister');
+// Registration routes
+Route::get('auth/register', [
+    'as' => 'register_path',
+    'uses' => 'Auth\AuthController@getRegister'
+]);
 Route::post('auth/register', 'Auth\AuthController@postRegister');
