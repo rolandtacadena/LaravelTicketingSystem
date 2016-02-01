28b5a4cbf20e15de9bdab3a64139c4a4d936e9e9
= <=>
2016-02-01 22:05:24 +0800

    Added method for uploading image

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -27,4 +27,26 @@ class UsersController extends Controller
     {
 
     }
+
+    public function upload_profile_photo(Request $request)
+    {
+        $this->validate($request, [
+            'profile-pic' => 'required|mimes:png'
+        ]);
+
+        // create a name for the profile photo
+        $imageName = $request->user()->id . '.' . $request->file('profile-pic')->getClientOriginalExtension();
+
+        // save the name on the users table
+        $user = $request->user();
+        $user->profile_photo = $imageName;
+        $user->save();
+
+        // move the temp uploaded file to /public/img/user/profile-pics/
+        $request->file('profile-pic')->move(base_path() . '/public/img/user/profile-pics/', $imageName);
+
+        return redirect('/user/profile')->with(
+            'flash_message', 'You have successfully added a photo!'
+        );
+    }
 }
